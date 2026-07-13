0c0685915a17231e57acd403121ded864d411f18
OpTiFall <62944006+OpTiFall@users.noreply.github.com>
2026-07-13 16:05:27 +0200

    scene_teleporter + attack_delay

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,37 +3,40 @@ class_name Enemy
 
 @export var speed: float;
 @export var gravity: float;
+@export var attack_delay: float;
 var ENEMY_SIZE: float = 32.0;
-var state = 'follow'
-var direction
+var state: String = 'follow'
+var direction: Vector2;
+
+var attack_timer;
 
 @onready var player: Player = get_tree().get_root().get_child(0).get_node("Player");
 
 func _physics_process(delta: float) -> void:
 	check_state()
+
 	if state == 'follow':
-		follow()
+		attack_timer = attack_delay;
+		direction = global_position.direction_to(player.global_position)
+		velocity = direction * ENEMY_SIZE * speed;
 	elif state == 'attack':
-		attack()
+		velocity = Vector2.ZERO
+		attack_timer -= delta;
+		if(attack_timer <= 0):
+			get_tree().reload_current_scene();
+
 	velocity.y += gravity * ENEMY_SIZE * delta;
 	handle_animation()
 	move_and_slide();
 
 func check_state():
-	if global_position.distance_to(player.global_position)>50:
+	if global_position.distance_to(player.global_position) > 50:
 		state = 'follow'
 	else:
 		state = 'attack'
-	
-func follow():
-	direction = global_position.direction_to(player.global_position)
-	velocity = direction * ENEMY_SIZE * speed;
-
-func attack():
-	velocity = Vector2.ZERO
 
 func handle_animation():
-	if direction.x <0 :
+	if direction.x < 0 :
 		%AnimatedSprite2D.flip_h = false
 	else:
 		%AnimatedSprite2D.flip_h = true
---
+++ scripts/scene_teleporter.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+const SCENES_PATH = "res://scenes/"
+@export var scene_name: String
+
+func _on_body_entered(body: Node2D) -> void:
+	if(body is Player):
+		if(scene_name == ""): print("ERROR: scene-name not defined in scene teleporter!!!"); return;
+		get_tree().change_scene_to_file(SCENES_PATH + scene_name + ".tscn");
+
