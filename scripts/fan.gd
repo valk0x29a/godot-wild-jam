42713bff7c5d0035920b8680c23401b5738817b3
Bad2d <moustafa.madani@icloud.com>
2026-07-13 22:04:46 +0300

    fan thing

--- scripts/fan.gd
+++ scripts/fan.gd
@@ -4,19 +4,18 @@ extends StaticBody2D
 @export var force: float;
 
 func _physics_process(_delta: float) -> void:
-    get_node("GPUParticles2D").global_rotation = global_rotation;
-    get_node("GPUParticles2D2").global_rotation = global_rotation;
-    get_node("GPUParticles2D3").global_rotation = global_rotation;
-    var nodes = trigger.get_overlapping_bodies();
-    print(nodes.size());
-    var direction = (trigger.get_node("CollisionShape2D").global_position - global_position).normalized();
-    print(direction);
-    for i in nodes.size():
-        if(nodes[i] is RigidBody2D):
-            var rigidbody: RigidBody2D = nodes[i];
-            rigidbody.apply_impulse(direction * force);
-        if(nodes[i] is CharacterBody2D):
-            var character: CharacterBody2D = nodes[i];
-            character.velocity += direction * force * 32;
-            character.move_and_slide();
-
+	%GPUParticles2D.global_rotation = global_rotation
+	%GPUParticles2D2.global_rotation = global_rotation
+	%GPUParticles2D3.global_rotation = global_rotation
+	var nodes = trigger.get_overlapping_bodies();
+	print(nodes.size());
+	var direction = (trigger.get_node("CollisionShape2D").global_position - global_position).normalized();
+	print(direction);
+	for i in nodes.size():
+		if(nodes[i] is RigidBody2D):
+			var rigidbody: RigidBody2D = nodes[i];
+			rigidbody.apply_impulse(direction * force);
+		if(nodes[i] is CharacterBody2D):
+			var character: CharacterBody2D = nodes[i];
+			character.velocity += direction * force * 32;
+			character.move_and_slide();
