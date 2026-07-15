9913ff51e301a9711f4bee1640b44fe75ad2f93d
OpTiFall <62944006+OpTiFall@users.noreply.github.com>
2026-07-15 22:26:34 +0200

    sounds working

--- scripts/sound_manager.gd
+++ scripts/sound_manager.gd
@@ -1,68 +1,47 @@
 extends Node
 
-# var jump_sound = preload("res://assets/audio/sfx/jump.wav");
-# var walk_sound = preload("res://assets/audio/sfx/jump.wav");
-var death_splash_sound = preload("res://assets/audio/sfx/squish.mp3");
-# var ui_button_sound = preload("res://assets/audio/sfx/jump.wav");
-# var ingame_button_sound = preload("res://assets/audio/sfx/jump.wav");
-# var fan_sound = preload("res://assets/audio/sfx/jump.wav");
-# var jumppad_sound = preload("res://assets/audio/sfx/jump.wav");
-# var barrel_bounce_sound = preload("res://assets/audio/sfx/jump.wav");
-# var bouncable_barrel_bounce_sound = preload("res://assets/audio/sfx/jump.wav");
-# var barrel_explosion_sound = preload("res://assets/audio/sfx");
-# var door_sound = preload("res://assets/audio/sfx");
-
-
-var audio: AudioStreamPlayer;
-
 func _ready() -> void:
-    audio = AudioStreamPlayer.new();
-    add_child(audio);
+	pass
+	get_tree().scene_changed.connect(stop_audio);
 
-# func play_jump():
-#     audio.stream = jump_sound;
-#     audio.play();
+func stop_audio():
+	for i in get_child_count():
+		var audio: AudioStreamPlayer = get_child(i);
+		audio.stop();
 
-# func play_walk():
-#     audio.stream = walk_sound;
-#     audio.play();
+func play_jump():
+	%jump.play();
+
+func play_walk():
+	%walk.play();
 
 func play_death_splash_sound():
-    audio.stream = death_splash_sound;
-    audio.play();
+	%death_splash.play();
 
-# func play_ui_button_sound():
-#     audio.stream = ui_button_sound;
-#     audio.play();
+func play_ui_button_sound():
+	%ui_button.play();
 
-# func play_ingame_button_sound():
-#     audio.stream = ingame_button_sound;
-#     audio.play();
+func play_ingame_button_sound():
+	%ingame_button.play();
 
-# func play_fan_sound():
-#     audio.stream = fan_sound;
-#     audio.play();
+func play_fan_sound():
+	%fan.play();
 
-# func play_jumppad_sound():
-#     audio.stream = jumppad_sound;
-#     audio.play();
+func play_jumppad_sound():
+	%jumppad.play();
 
-# func play_barrel_bounce_sound():
-#     audio.stream = barrel_bounce_sound;
-#     audio.play();
+func play_barrel_bounce_sound():
+	%barrel_bounce.play();
 
-# func play_bouncable_barrel_bounce_sound():
-#     audio.stream = bouncable_barrel_bounce_sound;
-#     audio.play();
+func play_bouncable_barrel_bounce_sound():
+	%bouncable_barrel_bounce.play();
 
-# func play_barrel_explosion_sound():
-#     audio.stream = barrel_explosion_sound;
-#     audio.play();
+func play_barrel_explosion_sound():
+	%barrel_explosion.play();
 
-# func play_door_sound():
-#     audio.stream = door_sound;
-#     audio.play();
+func play_door_sound():
+	%door.play();
 
-func play_audio_stream(stream: AudioStream):
-    audio.stream = stream;
-    audio.play();
+func play_music(stream: AudioStream):
+	%music_placeholder.stream = stream;
+	%music_placeholder.play();
