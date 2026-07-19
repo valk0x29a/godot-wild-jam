6afe802b1d9f2ce2464b1cc9b92c97ba99a505ab
OpTiFall <62944006+OpTiFall@users.noreply.github.com>
2026-07-19 19:05:24 +0200

    meow

--- scripts/fan.gd
+++ scripts/fan.gd
@@ -3,6 +3,12 @@ extends StaticBody2D
 @onready var trigger: Area2D = get_node("Fan Trigger");
 @export var force: float;
 
+func _ready() -> void:
+	SoundManager.play_fan_sound();
+
+func _exit_tree() -> void:
+	SoundManager.stop_fan_sound();
+
 func _physics_process(_delta: float) -> void:
 	var nodes = trigger.get_overlapping_bodies();
 	var direction = (trigger.get_node("CollisionShape2D").global_position - global_position).normalized();
